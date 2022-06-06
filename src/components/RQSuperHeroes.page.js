077b21f763bba418d03e9316b88483093875cc13
saniok333 <alex.o.csc@gmail.com>
2022-06-06 23:38:12 +0300

    Fetching data with useQuery

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,21 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
+
 export const RQSuperHeroesPage = () => {
-  return <div>RQ Super Heroes Page</div>;
+  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page</h2>
+      {data?.data.map((hero) => {
+        return <div>{hero.name}</div>;
+      })}
+    </>
+  );
 };
