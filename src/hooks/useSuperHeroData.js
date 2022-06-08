6aa671e8906d25c96ff6b06707187c36fc153861
saniok333 <alex.o.csc@gmail.com>
2022-06-08 14:54:37 +0300

    Query by Id 2

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,8 +1,10 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const fetchSuperHero = (id) =>
-  axios.get(`http://localhost:4000/superheroes/${id}`);
+const fetchSuperHero = ({ queryKey }) => {
+  const id = queryKey[1];
+  return axios.get(`http://localhost:4000/superheroes/${id}`);
+};
 
 export const useSuperHeroData = (id) =>
-  useQuery(['super-hero', id], () => fetchSuperHero(id));
+  useQuery(['super-hero', id], fetchSuperHero);
