3343bf4b1cf49354e43b217e26090e5f39a86aef
saniok333 <alex.o.csc@gmail.com>
2022-06-08 12:05:00 +0300

    Data Transformation

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { useState } from 'react';
 import { useQuery } from 'react-query';
 
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
@@ -7,15 +6,11 @@ const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
 export const RQSuperHeroesPage = () => {
   const onSuccessHandler = (data) => {
     console.log('Perform side effect after data fetching', data);
-    data.data.length > 3 && setRefetchIntervalValue(false);
   };
 
   const onErrorHandler = (error) => {
     console.log('Perform side effect after encountering error', error);
-    setRefetchIntervalValue(false);
   };
-
-  const [refetchIntervalValue, setRefetchIntervalValue] = useState(3000);
 
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     'super-heroes',
@@ -23,7 +18,7 @@ export const RQSuperHeroesPage = () => {
     {
       onSuccess: onSuccessHandler,
       onError: onErrorHandler,
-      refetchInterval: refetchIntervalValue,
+      select: (data) => data.data.map((hero) => hero.name),
     }
   );
 
@@ -41,9 +36,9 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>RQ Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {data?.data.map((hero) => {
-        return <div key={hero.id}>{hero.name}</div>;
-      })}
+      {data.map((heroName) => (
+        <div key={heroName}>{heroName}</div>
+      ))}
     </>
   );
 };
