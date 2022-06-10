7bb8f09d95cc745b614570af7b4722812c29a034
saniok333 <alex.o.csc@gmail.com>
2022-06-10 22:51:09 +0300

    Parallel Queries

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { HomePage } from './components/Home.page';
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page';
 import { SuperHeroesPage } from './components/SuperHeroes.page';
 import { RQSuperHeroPage } from './components/RQSuperHero.page';
+import { ParallelQueriesPage } from './components/ParallelQueries.page';
 
 const queryClient = new QueryClient();
 
@@ -28,6 +29,7 @@ const App = () => {
             </ul>
           </nav>
           <Routes>
+            <Route path="/rq-parallel" element={<ParallelQueriesPage />} />
             <Route
               path="/rq-super-heroes/:heroId"
               element={<RQSuperHeroPage />}
---
+++ src/components/ParallelQueries.page.js
@@ -0,0 +1,13 @@
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes');
+
+const fetchFriends = () => axios.get('http://localhost:4000/friends');
+
+export const ParallelQueriesPage = () => {
+  const { data: superHeroes } = useQuery('super-heroes', fetchSuperHeroes);
+  const { data: friends } = useQuery('friends', fetchFriends);
+
+  return <div>ParallelQueriesPage</div>;
+};
